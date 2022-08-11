cc9ad171f34288267fc5140717bd16fefac69dfd
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-08-11 23:07:36 +0430

    change color in bottom navigation

--- lib/views/bottom_navigation.dart
+++ lib/views/bottom_navigation.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../module/widgets/custom_icon.dart';
@@ -22,65 +24,74 @@ class _CustomBottmNavigationBarState extends State<CustomBottmNavigationBar> {
   Widget build(BuildContext context) {
     return Scaffold(
         bottomNavigationBar: Container(
-          decoration: BoxDecoration(color: Colors.grey.shade300),
-          child: BottomAppBar(
-            elevation: 0,
-            color: Colors.transparent,
-            child: SizedBox(
-              width: double.infinity,
-              height: 56,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CustomIcon(
-                    ontap: () {
-                      setState(() {
-                        _selectedIndex = 0;
-                      });
-                    },
-                    imgAddres: 'assets/svg/bottom_navigation_icon/home.svg',
-                    isSelected: _selectedIndex == 0,
-                    imgSelected:
-                        'assets/svg/bottom_navigation_icon/home_selected.svg',
+            decoration:
+                BoxDecoration(color: Color(0xFFFCFCFD).withOpacity(0.1)),
+            child: ClipRRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                child: BottomAppBar(
+                  elevation: 0,
+                  color: Colors.transparent,
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CustomIcon(
+                          ontap: () {
+                            setState(() {
+                              _selectedIndex = 0;
+                            });
+                          },
+                          imgAddres:
+                              'assets/svg/bottom_navigation_icon/home.svg',
+                          isSelected: _selectedIndex == 0,
+                          imgSelected:
+                              'assets/svg/bottom_navigation_icon/home_selected.svg',
+                        ),
+                        CustomIcon(
+                          imgSelected:
+                              "assets/svg/bottom_navigation_icon/favorite_selected.svg",
+                          ontap: () {
+                            setState(() {
+                              _selectedIndex = 1;
+                            });
+                          },
+                          imgAddres:
+                              'assets/svg/bottom_navigation_icon/favorite.svg',
+                          isSelected: _selectedIndex == 1,
+                        ),
+                        CustomIcon(
+                          ontap: () {
+                            setState(() {
+                              _selectedIndex = 2;
+                            });
+                          },
+                          imgAddres:
+                              'assets/svg/bottom_navigation_icon/chat.svg',
+                          isSelected: _selectedIndex == 2,
+                          imgSelected:
+                              'assets/svg/bottom_navigation_icon/chat_selected.svg',
+                        ),
+                        CustomIcon(
+                          ontap: () {
+                            setState(() {
+                              _selectedIndex = 3;
+                            });
+                          },
+                          imgAddres:
+                              'assets/svg/bottom_navigation_icon/user.svg',
+                          isSelected: _selectedIndex == 3,
+                          imgSelected:
+                              'assets/svg/bottom_navigation_icon/user_selecte.svg',
+                        )
+                      ],
+                    ),
                   ),
-                  CustomIcon(
-                    imgSelected:
-                        "assets/svg/bottom_navigation_icon/favorite_selected.svg",
-                    ontap: () {
-                      setState(() {
-                        _selectedIndex = 1;
-                      });
-                    },
-                    imgAddres: 'assets/svg/bottom_navigation_icon/favorite.svg',
-                    isSelected: _selectedIndex == 1,
-                  ),
-                  CustomIcon(
-                    ontap: () {
-                      setState(() {
-                        _selectedIndex = 2;
-                      });
-                    },
-                    imgAddres: 'assets/svg/bottom_navigation_icon/chat.svg',
-                    isSelected: _selectedIndex == 2,
-                    imgSelected:
-                        'assets/svg/bottom_navigation_icon/chat_selected.svg',
-                  ),
-                  CustomIcon(
-                    ontap: () {
-                      setState(() {
-                        _selectedIndex = 3;
-                      });
-                    },
-                    imgAddres: 'assets/svg/bottom_navigation_icon/user.svg',
-                    isSelected: _selectedIndex == 3,
-                    imgSelected:
-                        'assets/svg/bottom_navigation_icon/user_selecte.svg',
-                  )
-                ],
+                ),
               ),
-            ),
-          ),
-        ),
+            )),
         body: screan[_selectedIndex]);
   }
 }
